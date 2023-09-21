cb14835f477eb07617e1e674fce9356af2c876e3
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-09-21 13:53:40 -0500

    Updates jumbotron

--- src/routes/[chainId]/+page.svelte
+++ src/routes/[chainId]/+page.svelte
@@ -1,13 +1,15 @@
 <script lang=ts>
   import { page } from "$app/stores";
-	import { IconBook2, IconFlame } from "@tabler/icons-svelte";
+	import { IconBan, IconBook2, IconFlame, IconGasStation } from "@tabler/icons-svelte";
 </script>
 
 <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
       <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">HexPay.Day</h1>
       <p class="mb-4 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Transparent and audited contracts to manage your $HEX stakes efficiently and with ease.</p>
-      <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Stake solo, earn tips by ending stakes, start stakes for your cold wallet, just do it with less gas. ⛽🚫</p>
+      <p class="mb-4 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Stake solo, earn tips by ending stakes, start stakes for your cold wallet, onboard someone with a new stake for them... just do it with less gas.</p>
+      <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+        <IconGasStation class="inline-block" /><IconBan class="inline-block" /></p>
 
       <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a href="{$page.data.chainId}/start" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
