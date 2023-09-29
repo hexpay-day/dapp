f17bffca24aadcca691a9f193b42d27dc5e7094d
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-09-29 00:43:38 -0500

    Put back default schema

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const args = yargs().options({
   databaseSchema: {
     type: 'string',
     require: true,
-    default: 'hexpayday',
+    default: 'public',
   },
   hashSeed: {
     type: 'string',
