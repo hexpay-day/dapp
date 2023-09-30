5582001a89566aebe77511f15d35bd08f6d45e90
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-09-29 21:55:37 -0500

    Adds logo to static folder

--- src/stores/abis.ts
+++ src/stores/abis.ts
@@ -1,5 +1,4 @@
-import _IHEX from '@hexpayday/stake-manager/artifacts/contracts/reference/Hex.sol/HEX.json' assert { type: 'json' }
-import _GlobalsAndUtility from '@hexpayday/stake-manager/artifacts/contracts/reference/Hex.sol/GlobalsAndUtility.json' assert { type: 'json' }
+import _IHEX from '@hexpayday/stake-manager/artifacts/contracts/interfaces/IHEX.sol/IHEX.json' assert { type: 'json' }
 import _IHEXStakeInstanceManager from '@hexpayday/stake-manager/artifacts/contracts/interfaces/IHEXStakeInstanceManager.sol/IHEXStakeInstanceManager.json' assert { type: 'json' }
 import _StakeManager from '@hexpayday/stake-manager/artifacts/contracts/StakeManager.sol/StakeManager.json' assert { type: 'json' }
 import _Multicall from '@hexpayday/stake-manager/artifacts/contracts/test/IMulticall.sol/IMulticall3.json' assert { type: 'json' }
@@ -8,7 +7,6 @@ import _Perpetual from '@hexpayday/stake-manager/artifacts/contracts/test/MockPe
 export {
   _Perpetual as Perpetual,
   _IHEX as IHex,
-  _GlobalsAndUtility as GlobalsAndUtility,
   _IHEXStakeInstanceManager as IHEXStakeInstanceManager,
   _StakeManager as StakeManager,
   _Multicall as Multicall,
